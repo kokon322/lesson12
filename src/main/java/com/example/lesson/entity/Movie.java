9228e2dca0415f8ede75fa99e908aa1051a87558
kokon322 <chornuy322@gmail.com>
2021-01-13 18:08:32 +0100

    Initial commit

---
+++ src/main/java/com/example/lesson/entity/Movie.java
@@ -0,0 +1,23 @@
+package com.example.lesson.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String title;
+    private int duration;
+}
